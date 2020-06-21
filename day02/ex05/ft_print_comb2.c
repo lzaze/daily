9082c3ca65723f183f49f26c48fc91bffae7f5a4
luthando zaze <lzaze@c5r3s10.wethinkcode.co.za>
2020-06-21 09:47:01 +0200

    ex05

--- day02/ex05/ft_print_comb2.c
+++ day02/ex05/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: lzaze <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 09:50:57 by lzaze             #+#    #+#             */
-/*   Updated: 2020/06/19 10:54:35 by lzaze            ###   ########.fr       */
+/*   Updated: 2020/06/21 09:45:10 by lzaze            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,53 @@ void	ft_putchar(char c)
 
 void	ft_print_comb2(void)
 {
-	char	i;
-	char	j;
+	char	w;
+	char	x;
+	char	y;
+	char	z;
 
-	i = '0';
-	j = '0';
-	while (i <= '9')
+	w = '0';
+	while (w <= '9')
 	{
-		j = '0';
-		while (j <= '9')
+		x = '0';
+		while (x <= '8')
 		{
-			ft_putchar(i);
-			ft_putchar(j);
-
-			if (i == '9' && j == '9')
+			y = '0';
+			while (y <= '9')
 			{
-				ft_putchar('\n');
-				return;
+				z = x + 1;
+				while (z <= '9')
+				{
+					if (!(w == y && x == z))
+					{
+						ft_putchar(w);
+						ft_putchar(x);
+						ft_putchar(' ');
+						ft_putchar(y);
+						ft_putchar(z);
+						if (!(w == '9' && x == '8' && y == '9' && z =='9'))
+						{
+							ft_putchar(',');
+							ft_putchar(' ');
+						}
+						else
+						{
+							ft_putchar('\n');
+							return;
+						}
+					}
+					z++;
+				}
+				y++;
 			}
-			else
-			{
-				ft_putchar(',');
-				j++;
-			}
+			x++;
 		}
-		i++;
+		w++;
 	}
+}
+
+int		main()
+{
+	ft_print_comb2();
+	return 0;
 }
--- day02/ex07/ft_print_comb.c
+++ day02/ex07/ft_print_comb.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lzaze <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/19 11:11:11 by lzaze             #+#    #+#             */
-/*   Updated: 2020/06/20 10:48:09 by lzaze            ###   ########.fr       */
+/*   Created: 2020/06/19 11:58:46 by lzaze             #+#    #+#             */
+/*   Updated: 2020/06/19 13:24:09 by lzaze            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,5 @@
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);
-}
 
-void	ft_putnbr(int nb)
-{
-	int		i;
-	int 	tn;
-	int		number
-	tn = nb;
-	i = 0;
-	number = 0;
-	while (number == 0)
-	{
-		if (!(tn < 10))
-		{
-			tn = tn / 10;
-			i += 1;
-		}
-		else
-		{
-			number = 1;
-		}
-	}
-
-	while (nb[i] != '\0')
-	{
-		ft_putchar(nb[i]+48);
-		i++
-	}
-}
-
-int main()
-{
-	ft_putnbr(42);
-	return 0;
 }
