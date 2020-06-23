a8ed557cd5c2074ff11d5b9763e846ed2a5f8571
luthando zaze <lzaze@c5r3s10.wethinkcode.co.za>
2020-06-23 12:42:36 +0200

    ex00

--- day03/ex07/ft_strrev.c
+++ day03/ex07/ft_strrev.c
@@ -1,18 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lzaze <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/21 10:35:19 by lzaze             #+#    #+#             */
-/*   Updated: 2020/06/23 12:41:54 by lzaze            ###   ########.fr       */
+/*   Created: 2020/06/23 11:53:35 by lzaze             #+#    #+#             */
+/*   Updated: 2020/06/23 12:17:12 by lzaze            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+#include<stdio.h>
+void	*ft_strrev(char *str)
 {
-	int		nb;
-	nb = 42;
-	nbr = &nb; 	
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	int		j;
+	j = i;
+	while (i >= 0)
+	{
+		str[j] = str[i];
+		i--;
+	}
+	return (str);
+}
+
+int		main()
+{
+	char str;
+	ft_strrev("abc");
+	printf("%s\n",str);
+	return 0;
 }
